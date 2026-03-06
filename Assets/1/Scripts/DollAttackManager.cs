d6ebbc6f3a3aec1ef86a3f5fdb97a6565c3d7dc1
liaimv <liaimv0301@gmail.com>
2026-03-06 05:36:10 -0500

    Edit and add doll&player attack logic

--- Assets/1/Scripts/DollAttackManager.cs
+++ Assets/1/Scripts/DollAttackManager.cs
@@ -24,7 +24,7 @@ public class DollAttackManager : MonoBehaviour
     public float attackTimeRangeMin = 3f;
     public float attackTimeRangeMax = 7f;
 
-    private bool isAttacking = false;
+    public bool isAttacking = false;
 
     private GameObject selectedAttack1;
     private GameObject selectedAttack2;
@@ -35,8 +35,23 @@ public class DollAttackManager : MonoBehaviour
     private Image selectedAttack1RingImage;
     private Image selectedAttack2RingImage;
 
+    private Image selectedAttack1CircleImage;
+    private Image selectedAttack2CircleImage;
+
+    public ActiveAttack attack1;
+    public ActiveAttack attack2;
+
+    public bool attack1Frozen;
+    public bool attack2Frozen;
+
+    public Coroutine currentAttackCoroutine;
+
+    private PlayerAttackManager playerAttackManager;
+
     void Start()
     {
+        playerAttackManager = GetComponent<PlayerAttackManager>();
+
         headAttackUI.SetActive(false);
         //bodyAttackUI.SetActive(false);
         leftArmAttackUI.SetActive(false);
@@ -54,7 +69,7 @@ public class DollAttackManager : MonoBehaviour
             rightLegAttackUI
         };
 
-        StartCoroutine(AttackSequence(attackStartTime));
+        currentAttackCoroutine = StartCoroutine(AttackSequence(attackStartTime));
     }
 
     private IEnumerator AttackSequence(float attackTimeRange)
@@ -91,68 +106,158 @@ public class DollAttackManager : MonoBehaviour
     private IEnumerator StartAttack()
     {
         isAttacking = true;
-        Vector3 ringScale = new Vector3(ringSizeMax, ringSizeMax, ringSizeMax);
+        attack1Frozen = false;
+        attack2Frozen = false;
 
-        if (selectedAttack2 == null)
+        Vector3 ringScale1 = new Vector3(ringSizeMax, ringSizeMax, ringSizeMax);
+        Vector3 ringScale2 = new Vector3(ringSizeMax, ringSizeMax, ringSizeMax);
+
+        if (selectedAttack1 != null)
         {
             selectedAttack1.SetActive(true);
 
             selectedAttack1Ring = selectedAttack1.transform.GetChild(0).gameObject;
             selectedAttack1RingImage = selectedAttack1Ring.GetComponent<Image>();
+            selectedAttack1CircleImage = selectedAttack1.transform.GetChild(1).gameObject.GetComponent<Image>();
+            selectedAttack1RingImage.color = Color.white;
+            selectedAttack1CircleImage.color = Color.white;
+
+            attack1 = new ActiveAttack
+            {
+                attackObject = selectedAttack1,
+                ringObject = selectedAttack1Ring,
+                ringImage = selectedAttack1RingImage,
+                circleImage = selectedAttack1CircleImage,
+                attackName = GetKeyForAttack(selectedAttack1.name),
+                isGreen = false
+            };
         }
-        else
+
+        if (selectedAttack2 != null)
         {
-            selectedAttack1.SetActive(true);
             selectedAttack2.SetActive(true);
-
-            selectedAttack1Ring = selectedAttack1.transform.GetChild(0).gameObject;
             selectedAttack2Ring = selectedAttack2.transform.GetChild(0).gameObject;
-
-            selectedAttack1RingImage = selectedAttack1Ring.GetComponent<Image>();
             selectedAttack2RingImage = selectedAttack2Ring.GetComponent<Image>();
+            selectedAttack2CircleImage = selectedAttack2.transform.GetChild(1).gameObject.GetComponent<Image>();
+            selectedAttack2RingImage.color = Color.white;
+            selectedAttack2CircleImage.color = Color.white;
+
+            attack2 = new ActiveAttack
+            {
+                attackObject = selectedAttack2,
+                ringObject = selectedAttack2Ring,
+                ringImage = selectedAttack2RingImage,
+                circleImage = selectedAttack2CircleImage,
+                attackName = GetKeyForAttack(selectedAttack2.name),
+                isGreen = false
+            };
         }
 
         while (isAttacking)
         {
-            ringScale -= Vector3.one * Data.ringSpeed * Time.deltaTime;
-            if (ringScale.x <= ringSizeMin)
-            {
-                ringScale = Vector3.one * ringSizeMin;
+            bool attackEnded = false;
 
-                ResetVariables();
-            }
-            else if (ringScale.x <= 1.3f)
+            if (!attack1Frozen && selectedAttack1 != null)
             {
-                selectedAttack1RingImage.color = Color.green;
-                if (selectedAttack2 != null) selectedAttack2RingImage.color = Color.green;
+                ringScale1 -= Vector3.one * Data.ringSpeed * Time.deltaTime;
+
+                selectedAttack1Ring.transform.localScale = ringScale1;
+
+                if (ringScale1.x <= 1.3f)
+                {
+                    attack1.isGreen = true;
+                    selectedAttack1RingImage.color = Color.green;
+                }
+
+                if (ringScale1.x <= ringSizeMin)
+                {
+                    attack1Frozen = true;
+                    attackEnded = true;
+                    playerAttackManager.WrongTiming(attack1);
+                }
             }
 
-                selectedAttack1Ring.transform.localScale = ringScale;
-            if (selectedAttack2 != null) selectedAttack2Ring.transform.localScale = ringScale;
+            if (selectedAttack2 != null && !attack2Frozen)
+            {
+                ringScale2 -= Vector3.one * Data.ringSpeed * Time.deltaTime;
+
+                selectedAttack2Ring.transform.localScale = ringScale2;
+
+                if (ringScale2.x <= 1.3f)
+                {
+                    attack2.isGreen = true;
+                    selectedAttack2RingImage.color = Color.green;
+                }
+
+                if (ringScale2.x <= ringSizeMin)
+                {
+                    attack1Frozen = true;
+                    attackEnded = true;
+                    playerAttackManager.WrongTiming(attack2);
+                }
+            }
+
+            if (attackEnded)
+            {
+                isAttacking = false;
+            }
 
             yield return null; 
         }
     }
 
-    void ResetVariables()
+    public void EndCurrentAttack()
     {
-        selectedAttack1.SetActive(false);
+        if (selectedAttack1 != null)
+        {
+            selectedAttack1.SetActive(false);
+            selectedAttack1RingImage.color = Color.white;
+            selectedAttack1CircleImage.color = Color.white;
+            attack1.isGreen = false;
+            attack1Frozen = false;
+        }
+
         if (selectedAttack2 != null)
         {
             selectedAttack2.SetActive(false);
             selectedAttack2RingImage.color = Color.white;
+            selectedAttack2CircleImage.color = Color.white;
+            attack2.isGreen = false;
+            attack2Frozen = false;
         }
 
-        selectedAttack1RingImage.color = Color.white;
+        if (currentAttackCoroutine != null)
+        {
+            StopCoroutine(currentAttackCoroutine);
+            currentAttackCoroutine = null;
+        }
 
-        isAttacking = false;
-
-        float randomWaitTime = Random.Range(attackTimeRangeMin, attackTimeRangeMax);
-        StartCoroutine(AttackSequence(randomWaitTime));
+        float nextWait = Random.Range(attackTimeRangeMin, attackTimeRangeMax);
+        currentAttackCoroutine = StartCoroutine(AttackSequence(nextWait));
     }
 
-    void Update()
+    string GetKeyForAttack(string attackName)
     {
-        
+        switch (attackName)
+        {
+            case "HeadAttackUI": return "Head";
+            case "BodyAttackUI": return "Body";
+            case "LeftArmAttackUI": return "LeftArm";
+            case "RightArmAttackUI": return "RightArm";
+            case "LeftLegAttackUI": return "LeftLeg";
+            case "RightLegAttackUI": return "RightLeg";
+        }
+        return "";
     }
 }
+
+public class ActiveAttack
+{
+    public GameObject attackObject;
+    public GameObject ringObject;
+    public Image ringImage;
+    public Image circleImage;
+    public string attackName; 
+    public bool isGreen;
+}
+
