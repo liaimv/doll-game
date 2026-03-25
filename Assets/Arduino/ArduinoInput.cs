8dcbcc309d21f63b3b0c69fc1e4213abc7deaa6c
Ruoyu50 <sssssmess@foxmail.com>
2026-03-24 20:30:36 -0400

    dollDamegeWindow, comboWindow 0.2f->0.5f&ArduinoInput.cs updated

--- Assets/Arduino/ArduinoInput.cs
+++ Assets/Arduino/ArduinoInput.cs
@@ -8,6 +8,7 @@ public class ArduinoInput : MonoBehaviour
     public int leftArm;
     public int rightArm;
     public int leftLeg;
+    public int rightLeg;
     public int body;
 
     void Update()
@@ -17,21 +18,30 @@ public class ArduinoInput : MonoBehaviour
         if (message == null)
             return;
 
+        // Ardity系统消息过滤
+        if (message == SerialController.SERIAL_DEVICE_CONNECTED)
+            return;
+
+        if (message == SerialController.SERIAL_DEVICE_DISCONNECTED)
+            return;
+
         string[] values = message.Split(',');
 
-        if(values.Length == 5)
+        if(values.Length == 6)
         {
             head = int.Parse(values[0]);
             leftArm = int.Parse(values[1]);
             rightArm = int.Parse(values[2]);
             leftLeg = int.Parse(values[3]);
-            body = int.Parse(values[4]);
+            rightLeg = int.Parse(values[4]);
+            body = int.Parse(values[5]);
 
             Debug.Log(
                 "Head:" + head +
                 " LArm:" + leftArm +
                 " RArm:" + rightArm +
                 " LLeg:" + leftLeg +
+                " RLeg:" + rightLeg +
                 " Body:" + body
             );
         }
